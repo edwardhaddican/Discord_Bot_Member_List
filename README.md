6c64f082b23ef61419a75271a56af8725e302a79
Eddie <edwardhaddican@gmail.com>
2024-03-13 12:26:10 -0400

    A csv file that contains the username and id, can be downloaded

---
+++ README.md
@@ -0,0 +1,2 @@
+References: 
+https://medium.com/@idorenyinudoh10/how-to-export-data-from-javascript-to-a-csv-file-955bdfc394a9
--- commands/utility/allMembers.js
+++ commands/utility/allMembers.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require("discord.js");
 const { guildId } = require("../../config.json");
+const fs = require("node:fs");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -14,7 +15,10 @@ module.exports = {
     });
 
     console.log(members, "serveri info");
-    await interaction.reply({ content: 'Members have been retrieved' });
+
+    exportDataAsCSVFile(members);
+
+    await interaction.reply({ content: "Members have been retrieved" });
 
     // interaction.editReply({ embeds: [members] });
   },
@@ -25,6 +29,45 @@ async function getServerInfo(interaction) {
     const members = await interaction.guild.members.fetch();
 
     return members;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function exportDataAsCSVFile(membersArrOfObjects) {
+  console.log(membersArrOfObjects, "membersArrOfObjects");
+  try {
+    // Get the keys which will be used as the title, add them to an array and put that into the refined data in the first position
+    const titleKeys = Object.keys(membersArrOfObjects[0]);
+    const refinedData = [titleKeys];
+
+    // Loop over the Members Array, Use Object.values to get an array of the values and push that array into the refinedData array
+    membersArrOfObjects.forEach((e) => {
+      refinedData.push(Object.values(e));
+    });
+
+    // Now we need to create a string that will become our csvContent
+    let csvContent = "";
+
+    refinedData.forEach((row) => {
+      return (csvContent += row.join(",") + "\n");
+    });
+
+    // Next we need to create and configure a blob
+    // const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8," });
+    // const objUrl = URL.createObjectURL(blob);
+
+    // Now we need to download our file, we will use this
+
+    fs.writeFile("allMembersList.csv", csvContent, (error) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log("File saved successfully");
+      }
+    });
+
+
   } catch (error) {
     console.log(error);
   }
